6eff157eece6444078c71dedb3eba024f9485869
Fiki-josh <fikioluwamayomikun@gmail.com>
2023-12-29 21:01:57 +0100

    chore: images & icons upload

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="bg-blue-300 flex min-h-screen flex-col items-center justify-between p-24">
       <Button>Home</Button>
     </main>
   )
